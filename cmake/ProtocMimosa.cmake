a1b6ed139833ed2fa63dcfbe800253fc141eb101
Alexandre Bique <bique.alexandre@gmail.com>
2011-11-02 12:29:19 +0100

    Fixed protoc macro

--- cmake/ProtocMimosa.cmake
+++ cmake/ProtocMimosa.cmake
@@ -1,6 +1,6 @@
-macro(protoc_mimosa)
+macro(protoc_mimosa name)
   add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.h ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.cc
-    COMMAND protoc -mimosa_out ${CMAKE_CURRENT_SOURCE_DIR}/${name}.proto
+    COMMAND protoc --mimosa_out=. ${name}.proto
     DEPENDS ${name}.proto
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
   set_source_files_properties(
