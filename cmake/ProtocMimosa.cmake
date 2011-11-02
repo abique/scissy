f890141cf2c46082874d044ef567357c93a3451c
Alexandre Bique <bique.alexandre@gmail.com>
2011-11-02 12:22:35 +0100

    Trying to compile rpc protocol

---
+++ cmake/ProtocMimosa.cmake
@@ -0,0 +1,10 @@
+macro(protoc_mimosa)
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.h ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.cc
+    COMMAND protoc -mimosa_out ${CMAKE_CURRENT_SOURCE_DIR}/${name}.proto
+    DEPENDS ${name}.proto
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  set_source_files_properties(
+    ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.cc
+    PROPERTIES GENERATED true)
+endmacro(protoc_mimosa)
