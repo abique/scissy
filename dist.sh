63834a2d42da84c7ce3ddf79f50cc39705609ba5
Alexandre Bique <bique.alexandre@gmail.com>
2014-01-11 18:58:27 +0100

    Preparing package distribution

---
+++ dist.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+PROJECT=scissy
+VERSION=$(<VERSION)
+DISTDIR=$PROJECT-$VERSION
+DISTFILES='AUTHORS CMakeLists.txt Doxyfile ChangeLog dist.sh LICENSE README scissy VERSION mimosa www INSTALL'
+
+cd manual && make && cd -
+
+sed -i "s/PROJECT_NUMBER.*/PROJECT_NUMBER=$VERSION/g" Doxyfile
+rm -rf $DISTDIR
+mkdir $DISTDIR
+
+cp -R $DISTFILES $DISTDIR
+rm -rf $DISTDIR/www/bootstrap
+rm -rf $DISTDIR/www/update-bootstrap.sh
+rm -rf $DISTDIR/mimosa/.git*
+
+tar -cvvv $DISTDIR | xz -ze9c >$PROJECT-$VERSION.tar.xz
+rm -rf $DISTDIR
+
+md5sum $PROJECT-$VERSION.tar.xz >$PROJECT-$VERSION.tar.xz.md5sum
